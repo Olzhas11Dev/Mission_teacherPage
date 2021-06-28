c7c69df64cfc2ab42a7b2f6f3a0b8d827f98bec9
Olzhas <11olzhas.k@gmail.com>
2021-06-28 20:00:17 +1200

    updated

---
+++ src/components/PrgStudentSection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style/prg_student_section.css'
+const PrgStudentSection = () => {
+    return (
+    <div className="student_section">
+        <div className="topStudentContainer">
+            <div className="topStudentSection">
+             <h2>Beginner course</h2>
+            <div className="rightStudentSection">
+                <img src="" alt=""/>
+                <h3>EXPORT AS A SPREADSHEET</h3>
+            </div>
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default PrgStudentSection
